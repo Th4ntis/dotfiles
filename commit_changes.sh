220187ee5b4f8e914dcb1368e686634751fbe365
th4ntis <droneker@protonmail.com>
2022-02-15 02:11:21 -0500

    Sync dotfile configs

--- commit_changes.sh
+++ commit_changes.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 cd `dirname "$0"`
-cp $HOME/.zshrc .
-cp -R $HOME/.zsh .
-cp $HOME/.tmux.conf .
-cp -R $HOME/.tmux .
+cp $HOME/.zshrc ./zsh/
+cp -R $HOME/.zsh ./zsh/
+cp $HOME/.tmux.conf ./tmux/
+cp -R $HOME/.tmux ./tmux/
 git add --all
 git commit -m "Sync dotfile configs"
 git push origin
