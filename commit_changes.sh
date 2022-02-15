a218f2e7515d2daf836fd67c7498dce2cec0aee8
th4ntis <droneker@protonmail.com>
2022-02-15 02:17:15 -0500

    Sync dotfile configs

--- commit_changes.sh
+++ commit_changes.sh
@@ -4,6 +4,7 @@ cp $HOME/.zshrc ./zsh/
 cp -R $HOME/.zsh ./zsh/
 cp $HOME/.tmux.conf ./tmux/
 cp -R $HOME/.tmux ./tmux/
+cp -R $HOME/.config/fusuma ./fusuma/
 git add --all
 git commit -m "Sync dotfile configs"
 git push origin
--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 DIR_PATH=`dirname "$0"`
-fswatch -o $HOME/.zshrc $HOME/.zsh/ $HOME/.tmux.conf $HOME.tmux/ | xargs -n1 zsh $DIR_PATH/commit_changes.sh
+fswatch -o $HOME/.zshrc $HOME/.zsh/ $HOME/.tmux.conf $HOME/.tmux/ $HOME/.config/fusuma/ | xargs -n1 zsh $DIR_PATH/commit_changes.sh
