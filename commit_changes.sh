b317e99f8235aaa7ff89fc47c3ee172d4b50974d
th4ntis <droneker@protonmail.com>
2022-02-19 21:49:15 -0500

    Sync dotfile configs

--- commit_changes.sh
+++ commit_changes.sh
@@ -3,7 +3,6 @@ cd `dirname "$0"`
 cp $HOME/.zshrc ./zsh/
 cp -R $HOME/.zsh ./zsh/
 cp $HOME/.tmux.conf ./tmux/
-cp -R $HOME/.tmux ./tmux/
 cp $HOME/.vimrc ./vim/
 cp -R $HOME/.vim ./vim/
 cp -R $HOME/.config/fusuma ./fusuma/
