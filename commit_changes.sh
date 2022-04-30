ffd52d9a1a1f3f65fcce439b87bfa606a977c9c5
Th4ntis <53808039+Th4ntis@users.noreply.github.com>
2022-04-30 01:03:18 -0400

    Update commit_changes.sh

--- commit_changes.sh
+++ commit_changes.sh
@@ -1,10 +1,7 @@
 #!/bin/zsh
 cd `dirname "$0"`
 cp $HOME/.zshrc ./zsh/
-cp -R $HOME/.zsh ./zsh/
 cp $HOME/.tmux.conf ./tmux/
-cp $HOME/.vimrc ./vim/
-cp -R $HOME/.vim ./vim/
 cp -R $HOME/.config/fusuma ./fusuma/
 git add --all
 git commit -m "Sync dotfile configs"
