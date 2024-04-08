4029503431558df96fd2f17119bfb8843df7bfc2
Th4ntis <53808039+Th4ntis@users.noreply.github.com>
2024-04-08 14:11:37 +0000

    Update commit_changes.sh

--- commit_changes.sh
+++ commit_changes.sh
@@ -2,7 +2,9 @@
 cd `dirname "$0"`
 cp $HOME/.zshrc ./zsh/
 cp $HOME/.tmux.conf ./tmux/
+cp $HOME/.aliases ./aliases/
 cp -R $HOME/.config/fusuma ./fusuma/
+cp -R $HOME/.config/terminator ./terminator/
 git add --all
 git commit -m "Sync dotfile configs"
 git push origin
