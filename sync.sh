b083e2b61366b3bcc61045e0e606812d5322cbac
Th4ntis <53808039+Th4ntis@users.noreply.github.com>
2024-04-08 14:10:03 +0000

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 DIR_PATH=`dirname "$0"`
-fswatch -o $HOME/.zshrc $HOME/.tmux.conf $HOME/.config/fusuma/ | xargs -n1 zsh $DIR_PATH/commit_changes.sh
+fswatch -o $HOME/.zshrc $HOME/.aliases $HOME/.tmux.conf $HOME/.config/fusuma/ $HOME/.config/terminator/config | xargs -n1 zsh $DIR_PATH/commit_changes.sh
